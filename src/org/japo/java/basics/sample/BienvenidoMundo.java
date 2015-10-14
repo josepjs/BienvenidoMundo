addf84222ef827782a931f86404facd159d8ced8
josepsss <estaesunacuentaparawebs@gmail.com>
2015-10-14 12:19:33 +0200

    Cambiado a bocata de chorizo

--- src/org/japo/java/basics/sample/BienvenidoMundo.java
+++ src/org/japo/java/basics/sample/BienvenidoMundo.java
@@ -10,7 +10,7 @@ public class BienvenidoMundo {
 
    
     public static void main(String[] args) {
-       System.out.println("Bienvenido Mundo");
+       System.out.println("Bienvenido Bocata Chorizo");
     }
     
 }
